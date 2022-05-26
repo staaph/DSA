0f45bfa168b3c2f7534b5e826dd9114c7206cfa4
staaph <contact.staaph@gmail.com>
2022-05-26 18:44:04 +0200

    refactor

--- lessons/createArray.ts
+++ lessons/createArray.ts
@@ -1,29 +1,35 @@
 class MyArray {
   length: number;
   data: any;
+
   constructor() {
     this.length = 0;
     this.data = {};
   }
+
   get(index: number) {
     return this.data[index];
   }
+
   push(item: any) {
     this.data[this.length] = item;
     this.length++;
     return this.data;
   }
+
   pop() {
     const lastItem = this.data[this.length - 1];
     delete this.data[this.length - 1];
     this.length--;
     return lastItem;
   }
+
   deleteAtIndex(index: number) {
     const item = this.data[index];
     this.shiftItems(index);
     return item;
   }
+
   shiftItems(index: number) {
     if (index > this.length - 1 || index < 0) {
       return false;
