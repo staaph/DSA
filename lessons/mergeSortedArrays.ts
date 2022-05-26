e7fd7aca91a77371ff61bd676bc9c6d64b14d21b
staaph <contact.staaph@gmail.com>
2022-05-26 22:35:29 +0200

    added mergeSortedArrays

---
+++ lessons/mergeSortedArrays.ts
@@ -0,0 +1,30 @@
+const mergeSortedArrays = (array1: number[], array2: number[]) => {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item) {
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+};
+
+const mergeSortedArrays2 = (arr1: number[], arr2: number[]) =>
+  [...arr1, ...arr2].sort((a, b) => a - b);
